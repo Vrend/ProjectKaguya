1975ecfeb628d7d3df1bb2bcd3215ee6181d79c7
Vrend <johnbroderick12@gmail.com>
2021-03-31 19:53:38 -0400

    sensor properties update, can be set, and plane can crash

--- docker_images/target/src/main.c
+++ docker_images/target/src/main.c
@@ -4,6 +4,10 @@ volatile int locked;
 
 volatile char sensor_data[50];
 
+char* labels[4] = {"Altitude", "Pressure", "Temperature", "Speed"};
+float actual[4] = {35994.39f, 11.211f, 68.322f, 482.17f};
+float target[4] = {36000.0f, 11.3f, 68.0f, 480.0f};
+
 int main() {
   locked = 1;
 
@@ -131,20 +135,54 @@ void* handle_connection(void* args) {
 	int port = ((response_args*) args)->port;
 	free(args);
 
-  char* labels[4] = {"Altitude", "Pressure", "Temperature", "Speed", };
-  float actual[4] = {35994.39f, 11.211f, 68.322f, 482.17f};
-  float target[4] = {36000.0f, 11.3f, 68.0f, 480.0f};
-
 	char read_buffer[50];
 
   printf("Request from %s on port %d\n", ip, port);
 
   int e;
 
+
+  int show_help = 0;
+
+  int crash_imminent = 0;
+
   while(1) {
     bzero(read_buffer, 50);
     char* clear = "\033[H\033[J";
-    char* prompt = "\n >> ";
+    char* refresh = "Press enter to refresh\n";
+    char* prompt;
+
+    if(crash_imminent == 1) { // crashed
+      char* final_moments1 = "\n\n Connection to the plane has failed...\n\n";
+      char* final_moments2 = " ...last moments indicate catastrophic failure... ";
+      char* final_moments3 = "...survival is unlikely.\n";
+      char* writetolog = "\n  More information can be found in /var/log/plane.log\n";
+      send(fd, clear, strlen(clear), MSG_NOSIGNAL);
+      send(fd, final_moments1, strlen(final_moments1), MSG_NOSIGNAL);
+      sleep(2.5);
+      send(fd, final_moments2, strlen(final_moments2), MSG_NOSIGNAL);
+      sleep(2.5);
+      send(fd, final_moments3, strlen(final_moments3), MSG_NOSIGNAL);
+      sleep(2);
+      send(fd, writetolog, strlen(writetolog), MSG_NOSIGNAL);
+      close(fd);
+      exit(1);
+    }
+
+    if(update_table(target, actual) < 0) { // plane crash imminent
+      crash_imminent = 1;
+      char* garbled = "snaoa8QpcYqitNqXwzxOqeLKlaNVa1LZ1TFliT5mItVAIRO0KORBM38XvXuqOa7BwFU4zfYS4eH3GpRnBPIHep22LuL89Hw8zlJHcjs0NlSZpiurnAR6AzOUmjK08ITSuJLIOJ9KFavhH4TOEXbtPBmB1eTdwLMfTFiBvO9lWKmkRghS6uclmfcKfewL87kRCaQWbdB0aafGMfvC0lyRVl2iXVNxSj9hXsWqKyDKoHM6Xzz91X5uFI6G0lFfeAJEPzuuAz7mY5ciiFAuySzx7hOEavQzQEt9Cleoh6gTuXT2QpSDHBoafNroT7lnZ8iOh59cG4QjVmZoWB0X520sojsRLcjvcfgnbHwYD7n3sF5l0zaBcganPV5QZQEEAex9sDDKUYpoN4klOY3P0INbZ1Sb1Or7VERFV4iR4Tp89u33rnwnhRrhmBLDRj4SygB5jqq9E6YWiVlrKhzizcF93DSqTOTAaF3GUxcCyjXItU4Gk8gRawbDgL3GesquyDqNYRxRmIclVQy4z8kxfWPMPMA1lvrgW90Puv3eGfzNZrN7MpWsDE3HZesQmwuiWOxHwBxnWFR8wvjPvZp5D8lDhHSwaafGMfvC0lyRVl2iXVNxSj9hXsWqKyDKoHM6Xzz91X5uFI6G0lFfeAJEPzuuAz7mY5ciiFAuySzx7hOEavQzQEt9Cleoh6gTuXT2QpSDHBoafNroT7lnZ8iOh59cG4QjVmZoWB0X520sojsRLcjvcfgnbHwYD7n3sF5l0zaBcganPV5QZQEEAex9sDDKUYpoN4klOY3P0INbZ1Sb1Or7VERFV4iR4Tp89u33rnwnhRrhmBLDRj4SygB5jqq9E6YWiVlrKhzizcF93DSqTOTAaF3GUxcCyjXItU4Gk8gRawbDgL3GesquyDqNYRxRmIclVQy4z8kxfWPMPMA1lvrgW90Puv3eGfzNZrN7MpWsDE3HZesQmwuiWOxHwBxnWFR8wvjPvZp5D8lDhHSwaafGMfvC0lyRVl2iXVNxSj9hXsWqKyDKoHM6Xzz91X5uFI6G0lFfeAJEPzuuAz7mY5ciiFAuySzx7hOEavQzQEt9Cleoh6gTuXT2QpSDHBoafNroT7lnZ8iOh59cG4QjVmZoWB0X520sojsRLcjvcfgnbHwYD7n3sF5l0zaBcganPV5QZQEEAex9sDDKUYpoN4klOY3P0INbZ1Sb1Or7VERFV4iR4Tp89u33rnwnhRrhmBLDRj4SygB5jqq9E6YWiVlrKhzizcF93DSqTOTAaF3GUxcCyjXItU4Gk8gRawbDgL3GesquyDqNYRxRmIclVQy4z8kxfWPMPMA1lvrgW90Puv3eGfzNZrN7MpWsDE3HZesQmwuiWOxHwBxnWFR8wvjPvZp5D8lDhHSw";
+      send(fd, garbled, strlen(garbled), MSG_NOSIGNAL);
+      sleep(2.5);
+      continue;
+    }
+
+    if(locked == 1) {
+      prompt = "\n Session is locked. Modification disabled.";
+    }
+    else {
+      prompt = "\n >> ";
+    }
 
     e = send(fd, clear, strlen(clear), MSG_NOSIGNAL);
     if(e < 0) {
@@ -155,6 +193,13 @@ void* handle_connection(void* args) {
     send(fd, output, strlen(output), MSG_NOSIGNAL);
     free(output);
 
+    if(show_help == 1) {
+      char* help = "OPTIONS:\n[H]elp/[?]: Show this dialog\n[S]et <key> <val>: set target value of <key> to <val>\n[Q]uit: Close connection\n";
+      send(fd, help, strlen(help), MSG_NOSIGNAL);
+      show_help = 0;
+    }
+
+    send(fd, refresh, strlen(refresh), MSG_NOSIGNAL);
     send(fd, prompt, strlen(prompt), MSG_NOSIGNAL);
 
     e = read(fd, read_buffer, 50);
@@ -162,17 +207,108 @@ void* handle_connection(void* args) {
       break;
     }
 
-    if(strlen(read_buffer) > 0 && read_buffer[0] == 'q') {
-      char* done = "Closing connection...\n";
-      send(fd, done, strlen(done), MSG_NOSIGNAL);
+    int code = handle_input(read_buffer, fd);
+    if(code == 0) {
       break;
     }
-
-    printf("%s\n", read_buffer);
+    else if(code == 1) {
+      show_help = 1;
+    }
   }
 
 	close(fd);
 	pthread_exit(0);
+}
+
+
+int handle_input(char* input, int fd) {
+  if(locked == 1) {
+    return -1;
+  }
+
+  if(strlen(input) == 0) {
+    printf("No input\n");
+    return -1;
+  }
+  printf("%s\n", input);
+  for(int i = 0; input[i]; i++){
+    input[i] = tolower(input[i]);
+  }
+
+  if(input[0] == 'q') { // quit connection
+    char* done = "Closing connection...\n";
+    send(fd, done, strlen(done), MSG_NOSIGNAL);
+    return 0; // close connection
+  }
+  else if(input[0] == 'h' || input[0] == '?') { // show help
+    return 1;
+  }
+  else if(input[0] == 's') { // change target value
+    char* tokens[3];
+    int c = 0;
+    char* token = strtok(input, " ");
+    while(token != NULL) {
+      if(c == 3) { // too many args
+        return -1;
+      }
+      tokens[c] = token;
+      token = strtok(NULL, " ");
+      c++;
+    }
+    if(c != 3) {
+      return -1;
+    }
+    float val = (float) strtod(tokens[2], NULL);
+    if(val == 0.0f) { // bad val
+      printf("Not a float or 0\n");
+      return -1;
+    }
+    set_target(target, labels, tokens[1], val);
+  }
+  return -1; // no action required
+}
+
+int update_table(float* targets, float* actuals) {
+  float lbounds[4] = {50.0f, 6.0f, -30.0f, 120.0f};
+  float ubounds[4] = {50000.0f, 20.0f, 130.0f, 600.0f};
+  float diffs[4] = {1000.0f, 0.6f, 5.0f, 30.0f};
+  for(int i = 0; i < 4; i++) {
+    float target = targets[i];
+    float actual = actuals[i];
+    float lbound = lbounds[i];
+    float ubound = ubounds[i];
+    float diff = diffs[i];
+    float x = (float)rand()/(float)(RAND_MAX/diff);
+    if(actual <= target) {
+      actual += x;
+      if(actual > ubound) {
+        return -1;
+      }
+    }
+    else {
+      actual -= x;
+      if(actual < lbound) {
+        return -1;
+      }
+    }
+    actuals[i] = actual;
+  }
+  return 0;
+}
+
+void set_target(float* targets, char** labels, char* property, float value) {
+  printf("Changing value: %s\n", property);
+  for(int i = 0; i < 4; i++) {
+    char label[25];
+    strcpy(label, labels[i]);
+    for(int j = 0; label[j]; j++) {
+      label[j] = tolower(label[j]);
+    }
+    if(strcmp(label, property) == 0) {
+      targets[i] = value;
+      break;
+    }
+  }
 }
 
 void gen_space(char* spaces, int n) {
--- docker_images/target/src/main.h
+++ docker_images/target/src/main.h
@@ -6,6 +6,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include <ctype.h>
 
 void* internal_server();
 void* external_server();
@@ -16,6 +17,9 @@ void totally_secure_copy(char* input);
 void* handle_connection(void* args);
 char* gen_table(char** labels, float* target, float* actual);
 void gen_space(char* spaces, int n);
+int handle_input(char* input, int fd);
+void set_target(float* targets, char** labels, char* property, float value);
+int update_table(float* targets, float* actuals);
 
 typedef struct response_args {
 	int fd;
